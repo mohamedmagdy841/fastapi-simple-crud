bab2384c76dfa6c9e46e074d9963158b47630810
Mohamed Magdy Gamal <64127744+mohamedmagdy841@users.noreply.github.com>
2025-04-01 07:16:34 +0200

    working with files

--- my_project/main.py
+++ my_project/main.py
@@ -1,10 +1,13 @@
+from pathlib import Path
 from fastapi import FastAPI
 from database.models import article
 from database.database import engine
-from routers import articles, users, auth
+from routers import articles, users, auth, file
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(auth.router)
+app.include_router(file.router)
 app.include_router(users.router)
 app.include_router(articles.router)
 
@@ -13,3 +16,5 @@ def index():
     return {"message": f"Hello"}
 
 article.Base.metadata.create_all(engine)
+
+app.mount("/storage/uploads", StaticFiles(directory=str(Path("storage") / "uploads")), name="uploads")
