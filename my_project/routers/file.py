b0e5cb804ef24c79fcbb05ec78ab45417e5d253e
Mohamed Magdy Gamal <64127744+mohamedmagdy841@users.noreply.github.com>
2025-04-01 07:18:13 +0200

    working with files

---
+++ my_project/routers/file.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, File, UploadFile, Request, HTTPException, status
+from fastapi.responses import FileResponse
+import shutil
+import hashlib
+import time
+from pathlib import Path
+
+router = APIRouter(prefix='/files', tags=["files"])
+
+# Define storage directory
+UPLOAD_DIR = Path("storage/uploads")
+UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def generate_unique_image(filename: str) -> str:
+    ext = filename.split(".")[-1]
+    timestamp = str(int(time.time()))
+    hash_part = hashlib.sha256(filename.encode()).hexdigest()[:10]  # Short hash
+    return f"{hash_part}_{timestamp}.{ext}"
+
+@router.post("/upload")
+def upload_file(request: Request, file: UploadFile = File(...)):
+    new_filename = generate_unique_image(file.filename)
+    path = UPLOAD_DIR / new_filename
+
+    with open(path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    base_url = str(request.base_url)  # Example: "http://127.0.0.1:8000/"
+
+    return {"filename": new_filename, "url": f"{base_url}files/{new_filename}"}
+
+@router.get("/download/{filename}", response_class=FileResponse)
+def get_file(filename: str):
+    path = UPLOAD_DIR / filename
+    if not path.exists():
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="File not found")
+    return path
+
